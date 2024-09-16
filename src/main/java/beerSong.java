294552713773a5ad6065af6f4b776b6dbe583242
Belousova Elina <Elksy90@gmail.com>
2024-09-16 18:11:25 +0400

    Added beerSong

---
+++ src/main/java/beerSong.java
@@ -0,0 +1,26 @@
+public class beerSong {
+
+    public static void main(String[] args) {
+        int beerNum = 99;
+        String word = "bottles";
+
+        while (beerNum > 0) {
+
+            System.out.println(beerNum + " " + word + " of beer on the wall, " + beerNum + " " + word + " of beer.");
+            System.out.print("Take one down and pass it around, ");
+            beerNum = beerNum - 1;
+
+            if (beerNum == 1) {
+                word = "bottle";
+            }
+
+            if (beerNum > 0) {
+                System.out.println(beerNum + " " + word + " of beer on the wall.");
+            }
+            else {
+                System.out.println("No more bottles of beer on the wall.");
+            }
+        }
+    }
+}
+
